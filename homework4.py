b840ef45016bfd53f49028134f634c5b71670520
Piyanat Traipisut <150587777+DeefseenNaja@users.noreply.github.com>
2024-01-14 19:44:39 +0700

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,43 @@
+print("hello world") 
+print("I don't have a car")
+print("You got a new job!? That’s so exciting")
+print("a\nan\nthe")
+print("Just because something\nthink different from you\ndoes that mean it's not thinking?")
+
+a = 12.5
+print("a = %.1f" % a)
+
+a = 2
+b = 3
+print("%d"% (a*b))
+
+a = 2.4
+b = 2.5
+print("%f" % (a+b))
+
+a = 5
+b = 2
+print("%f" % (a-b))
+
+birthday = 25
+print("ฉันเกิดวันที่%d" %(birthday))
+
+print("good morning")
+
+print("*\n**\n***")
+
+#a = int(input())
+#b = int(input())
+#print("%d" % (a+b))
+
+#strl = (input())
+#n = int(input())
+#print(strl * n)
+#print = ("result")
+
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#avg = ((a+b+c) / 3)
+#print(avg)
+
